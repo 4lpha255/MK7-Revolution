00539ddf908a5df780b02dbde20490f09264866d
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-07-23 13:34:22 +0200

    Update Network - Kart Warp

--- src/base/features/network/kart_warp.cpp
+++ src/base/features/network/kart_warp.cpp
@@ -4,13 +4,11 @@
 
 #include <Kart/NetData.hpp>
 
-#define WARP 0x20000
-
 namespace base
 {
     void features::network::kart_warp(Kart::NetData *_this)
     {
         if (g_menu->m_kart_warp_entry->IsActivated())
-            _this->kart_data ^= WARP;
+            _this->kart_data.warp ^= true;
     }
 }
