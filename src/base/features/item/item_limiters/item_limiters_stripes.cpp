2479c14714b594a5e3c00e7c27cbfb59b94e2a31
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-08-19 11:56:35 +0200

    Update item_limiters_stripes.cpp

--- src/base/features/item/item_limiters/item_limiters_stripes.cpp
+++ src/base/features/item/item_limiters/item_limiters_stripes.cpp
@@ -11,24 +11,20 @@ namespace base
         if (g_menu->m_item_limiters_entry->IsActivated())
         {
             // Calculate the amount of stripes to allocate (limited and original); original amount is hardcoded and based on EItemPattern.
-            auto const is_shells_mode = max == 47;
+            auto const is_shell_pattern = max == 47;
             auto const data = std::map<Item::eItemType, s32>
             {
-                { Item::eItemType::KouraG, is_shells_mode ? 23 : 11 },
-                { Item::eItemType::KouraR, is_shells_mode ? 23 : 11 },
+                { Item::eItemType::KouraG, is_shell_pattern ? 23 : 11 },
+                { Item::eItemType::KouraR, is_shell_pattern ? 23 : 11 },
                 { Item::eItemType::KouraB, 1 },
             };
 
-            auto result = s32{};
-
+            max = 0;
             for (auto const &e : data)
                 if (auto const amount = num(e.first); amount != -1)
-                    result += amount;
+                    max += amount;
                 else
-                    result += e.second;
-
-            if (result != 0)
-                max = result;
+                    max += e.second;
         }
     }
 }
