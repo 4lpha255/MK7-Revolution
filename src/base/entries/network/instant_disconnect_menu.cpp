1237eff4daea98905e138a57e866e5051053adf9
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-09-11 22:04:03 +0200

    Update Network - Instant Disconnect
    Allows undoing the action and also prints minimal information

--- src/base/entries/network/instant_disconnect_menu.cpp
+++ src/base/entries/network/instant_disconnect_menu.cpp
@@ -9,6 +9,17 @@ namespace base
 {
     void entries::network::instant_disconnect_game(CTRPluginFramework::MenuEntry *entry)
     {
-        (*g_pointers->m_network_engine)->m_network_error_handler->m_error_code = 0xB;
+        auto &code = (*g_pointers->m_network_engine)->m_network_error_handler->m_error_code;
+
+        if (code == 0)
+        {
+            code = 0xB;
+            CTRPluginFramework::MessageBox(entry->Name(), "Done.")();
+        }
+        else
+        {
+            code = 0;
+            CTRPluginFramework::MessageBox(entry->Name(), "Reset.")();
+        }
     }
 }
