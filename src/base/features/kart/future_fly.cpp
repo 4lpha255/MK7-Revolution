5b709230ed5b600bb755d58f301953c203235af4
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-11-24 12:15:43 +0100

    Update future_fly.cpp

--- src/base/features/kart/future_fly.cpp
+++ src/base/features/kart/future_fly.cpp
@@ -9,29 +9,31 @@
 
 namespace base
 {
-
     void features::kart::future_fly::calc(game::kart::unit *_this)
     {
-        if (auto const &veh = _this->m_vehicle; g_menu->m_future_fly_entry->IsActivated() && (veh->m_is_master && !veh->m_is_net_recv))
-        {
-            auto const &future_fly = g_settings.m_options.kart.future_fly;
+        if (g_menu->m_future_fly_entry->IsActivated())
+        {            
+            if (auto const &veh = _this->m_vehicle; veh->m_is_master && !veh->m_is_net_recv)
+            {
+                auto const &future_fly = g_settings.m_options.kart.future_fly;
 
-            veh->m_status_flags.ground_camera = true;
+                veh->m_status_flags.ground_camera = true;
 
-            // Stay frozen in place
-            veh->m_forward_speed = 0.f;
-            veh->m_velocity = sead::Vector3f::zero;
+                // Stay frozen in place
+                veh->m_forward_speed = 0.f;
+                veh->m_velocity = sead::Vector3f::zero;
 
-            // Allow yaw rotation
-            veh->m_yaw_strength = veh->m_cpad_x * (!veh->m_controls.accelerate_backwards ? -future_fly.yaw_strength : future_fly.yaw_strength);
+                // Allow yaw rotation
+                veh->m_yaw_strength = veh->m_cpad_x * (!veh->m_controls.accelerate_backwards ? -future_fly.yaw_strength : future_fly.yaw_strength);
 
-            // Allow pitch rotation
-            auto const pitch = veh->m_cpad_y * future_fly.pitch_limit;
-            veh->m_front_pitch = sead::Vector3f{ pitch * veh->m_up.x, future_fly.pitch_keep ? pitch * veh->m_up.y : veh->m_front_pitch.y, pitch * veh->m_up.z };
-            
-            // Allow movement
-            if (veh->m_controls.accelerate_forwards || veh->m_controls.accelerate_backwards)
-                *veh->m_position += veh->m_up * (!veh->m_controls.accelerate_backwards ? future_fly.speed_forward : -future_fly.speed_backward);
+                // Allow pitch rotation
+                auto const pitch = veh->m_cpad_y * future_fly.pitch_limit;
+                veh->m_front_pitch = sead::Vector3f{ pitch * veh->m_up.x, future_fly.pitch_keep ? pitch * veh->m_up.y : veh->m_front_pitch.y, pitch * veh->m_up.z };
+                
+                // Allow movement
+                if (veh->m_controls.accelerate_forwards || veh->m_controls.accelerate_backwards)
+                    *veh->m_position += veh->m_up * (!veh->m_controls.accelerate_backwards ? future_fly.speed_forward : -future_fly.speed_backward);
+            }
         }
     }
 
